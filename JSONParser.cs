e0449c0b5dc90ba867e82669b1b94c345495f3c9
Ryan Ploetz <rploetz@somnio.com>
2012-10-30 16:56:04 -0500

    Update JSONParser.cs

--- JSONParser.cs
+++ JSONParser.cs
@@ -5,9 +5,7 @@ MIT LICENSE
 Copyright (C) 2012 - Ryan Ploetz
 
 LinkedIn: http://www.linkedin.com/in/ploetz
-Twitter: @baygullstudios
-Email: rploetz@baygullstudios.com
-Website: www.baygullstudios.com
+Email: rploetz@somnio.com
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
